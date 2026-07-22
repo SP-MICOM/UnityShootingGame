2dcbbaa0ac0e15ec2055a40ce5940db2fde5c2ff
SP-MICOM <superfamicom1022@gmail.com>
2026-07-22 21:15:00 +0900

    (2026.7.22)

--- Program/Assets/Scripts/Character/Character.cs
+++ Program/Assets/Scripts/Character/Character.cs
@@ -11,10 +11,13 @@ public class Character : MonoBehaviour
     [SerializeField] Quaternion quaternion;
     [SerializeField] Vector3 vector3;
     [SerializeField] Rigidbody rigidbody;
-
-    private bool isRolling = false;
-    private float comboTime = 0f;
+    [SerializeField] bool isRolling = false;
+    [SerializeField] float rollingComboTime = 0f;
+    [SerializeField] float rollingCoolTime = 0f;
     
+    private static float comboTime = 0.5f;
+    private static float coolTime = 0.5f;
+
     private void Start()
     {
         speed = 20f;
@@ -30,7 +33,7 @@ public class Character : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
-
+        Rolling();
         Rotate();
     }
 
@@ -41,20 +44,36 @@ public class Character : MonoBehaviour
 
         quaternion = Quaternion.Euler(0, 0, Input.GetAxis("Horizontal") * -speed);
 
+        // 입력 확인
         if (Input.GetKey(KeyCode.E))
         {
             quaternion = Quaternion.Euler(0, 0, -90);
-            comboTime = Time.time + 0.25f;
         }
         else if (Input.GetKey(KeyCode.Q))
         {
             quaternion = Quaternion.Euler(0, 0, 90);
         }
+
+        // 입력 취소 확인
+        if (Input.GetKeyUp(KeyCode.E) || Input.GetKeyUp(KeyCode.Q))
+        {
+            rollingComboTime = Time.time + comboTime;
+        }
     }
 
-    public void Check()
+    public bool CheckCombo()
     {
+        if(Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Q))
+        {
+            if(Time.time < rollingComboTime)
+            {
+                rollingCoolTime = Time.time + coolTime;
 
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public void Move()
@@ -64,9 +83,26 @@ public class Character : MonoBehaviour
 
     public void Rotate()
     {
-        rigidbody.transform.rotation = Quaternion.Slerp(transform.rotation, quaternion, Time.deltaTime * speed);
+        if (!isRolling)
+        {
+            rigidbody.transform.rotation = Quaternion.Slerp(transform.rotation, quaternion, Time.deltaTime * speed);
+        }
     }
 
+    public void Rolling()
+    {
+        if (Time.time > rollingCoolTime)
+        {
+            isRolling = false;
+        }
+
+        if(CheckCombo() && !isRolling)
+        {
+            Debug.Log("Rolling");
+
+            isRolling = true;
+        }
+    }
 
     public void Pause()
     {
